2748187b09a711be51c1cd0278f5b81b85cb3082
n11426764 <n11426764@qut.edu.au>
2024-12-02 04:03:52 +1000

    Initial Commit

---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by Masahiro Yasuda on 2/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
