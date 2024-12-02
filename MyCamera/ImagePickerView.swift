7a96fc81007935b3c7a19bc69096f6be23d1a197
n11426764 <n11426764@qut.edu.au>
2024-12-02 11:06:53 +1000

    initi

---
+++ MyCamera/ImagePickerView.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePickerView.swift
+//  MyCamera
+//
+//  Created by Masahiro Yasuda on 2/12/2024.
+//
+
+import SwiftUI
+
+struct ImagePickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePickerView()
+}
---
+++ MyCamera/UIImageExtension.swift
@@ -0,0 +1,18 @@
+//
+//  UIImageExtension.swift
+//  MyCamera
+//
+//  Created by Masahiro Yasuda on 2/12/2024.
+//
+
+import SwiftUI
+
+struct UIImageExtension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIImageExtension()
+}
